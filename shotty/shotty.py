ccea6a9d35e043110c812e1cb2ba4b62db2e366e
justing4212 <jag395@bu.edu>
2018-09-21 23:42:06 -0400

    Added volumes and snapshots to script

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -14,6 +14,54 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """Shotty interface"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help='Only snapshots for (tag Project:<name>)')
+def list_volumes(project):
+    "List EC2 snapshots"
+    instances = filter_instances(project)
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(', '.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime('%c')
+                )))
+    return
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help='Only volumes for (tag Project:<name>)')
+def list_volumes(project):
+    "List EC2 volumes"
+    instances = filter_instances(project)
+    for i in instances:
+        for v in i.volumes.all():
+            print(', '.join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size)+'GiB',
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
 
@@ -57,5 +105,18 @@ def start_instances(project):
         i.start()
     return
 
+@instances.command('snapshot',
+    help='Create snapshots of all volumes')
+@click.option('--project', default=None,
+    help='Only instances for project')
+def create_snapshots(project):
+    "Create EC2 snapshots"
+    instances = filter_instances(project)
+    for i in instances:
+        for v in i.volumes.all():
+            print('Creating snapshot of {0}'.format(v.id))
+            v.create_snapshot(Description='Create by SnapshotDemo')
+        return
+
 if __name__ == '__main__':
-    instances()
+    cli()
